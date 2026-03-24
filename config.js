d7195ddd2a113b447b43ed34cb0f4e28ae0d10f4
useridk758 <9z8r9zwdsh@privaterelay.appleid.com>
2026-03-24 10:40:29 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/service/', // Changed from '/proxy/'
+    bare: 'https://your-server-link.com', 
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
